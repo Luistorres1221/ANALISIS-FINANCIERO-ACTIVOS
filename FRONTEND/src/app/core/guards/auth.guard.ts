59b7157f6e94e9dad0f8b06f84e93bb14d311a9f
Colaborador <colaborador@uniquindio.edu.co>
2026-05-13 12:11:59 -0500

    fix: Remover duplicado de 'inject' en auth.guard.ts

--- FRONTEND/src/app/core/guards/auth.guard.ts
+++ FRONTEND/src/app/core/guards/auth.guard.ts
@@ -1,13 +1,6 @@
-import { Injectable, inject } from '@angular/core';
+import { inject } from '@angular/core';
 import { Router, CanActivateFn } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard {
-  constructor(private router: Router, private authService: AuthenticationService) {}
-}
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
